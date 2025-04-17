fba5ce0d5b8d73eec1fab470aec506f64359c1ab
Ruth Canchanya Hurtado <rcanchan@c2r18s1.42madrid.com>
2025-04-17 19:41:05 +0200

    well done

--- Rank_02_Ruth/L1/rev_print/rev_print.c
+++ Rank_02_Ruth/L1/rev_print/rev_print.c
@@ -1,12 +1,12 @@
 #include <unistd.h>
 char *rev_print(char *str)
 {
-    int i = 0;
-    while(str[i])
-    i++;
-    while(i-- >= 0)
-        write (1, &str[i], 1);
-    return (str);
+  int i = 0;
+  while (str[i])
+  i++;
+  while (str[i] >= 0)
+    write(1, &str[i], 1);
+  return(str);
 }
 
 int main(void)
